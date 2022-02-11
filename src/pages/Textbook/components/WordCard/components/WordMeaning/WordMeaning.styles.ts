6b94eb490ebfc026b3c216c00f4441e4e8d4c242
fgriff <anglirum@gmail.com>
2022-02-11 12:42:27 +0300

    fix: word highlight style

--- src/pages/Textbook/components/WordCard/components/WordMeaning/WordMeaning.styles.ts
+++ src/pages/Textbook/components/WordCard/components/WordMeaning/WordMeaning.styles.ts
@@ -3,4 +3,14 @@ import styled from 'styled-components';
 export const StyledWordMeaning = styled.div`
   display: flex;
   flex-direction: column;
+
+  p {
+    i {
+      font-style: italic;
+    }
+
+    b {
+      font-weight: bold;
+    }
+  }
 `;
