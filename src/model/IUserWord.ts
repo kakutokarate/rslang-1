e9f5361a7f2373055119d5b9f9c167d44bc9d10c
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-02-14 02:51:42 +0300

    refactor: extend interfaces IWord and IUserWord

--- src/model/IUserWord.ts
+++ src/model/IUserWord.ts
@@ -1,4 +1,20 @@
+type TDifficulty = 'difficult' | '';
+
 export interface IUserWord {
-  difficulty: string;
-  optional: {};
+  difficulty: TDifficulty;
+  optional?: {
+    startDate: string; // строка в формате ISO для подсчета статистики кол-во новых слов за день
+    isLearned: boolean;
+    counter: number; // для выставления признака изученного слова, инкремент при правильном ответе, обнуление при неправильном
+    games: {
+      sprint: {
+        correct: number;
+        wrong: number;
+      };
+      challenge: {
+        correct: number;
+        wrong: number;
+      };
+    };
+  };
 }
