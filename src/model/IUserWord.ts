fc01b42554fe2c86e73616982e31632d4a85f320
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-02-18 03:45:00 +0300

    refactor: add ILocalStatistic, update IUserWord and IStatistic

--- src/model/IUserWord.ts
+++ src/model/IUserWord.ts
@@ -1,12 +1,26 @@
-type TDifficulty = 'difficult' | '';
+type TDifficulty = 'difficult' | 'easy';
 
 export interface IUserWord {
   difficulty: TDifficulty;
   wordId?: string;
   userId?: string;
   optional?: {
-    startDate: string; // строка в формате ISO для подсчета статистики кол-во новых слов за день
-    isLearned: boolean;
+    startStudying: string | null; // строка в формате ISO для подсчета статистики кол-во новых слов за день
+    isLearned: boolean; // 3 попытки угаданы правильно подряд
+    // gamesData: [
+    //   {
+    //     date: '16-02-2022'; //5e9f5ee35eb9e72bc21af6fa
+    //     game: 'sprint'; //user 6209ab25b45ca9001665429f
+    //     result: true; // если на указанную дату уже стоит true - не меняем, для процент правильных ответов
+    //     correctCounter: 3; // если больше 0 - для статистики считаем изученным
+    //     wrongCounter: 2;
+    //   },
+    //   {
+    //     date: '16-02-2022'; //5e9f5ee35eb9e72bc21af6fa
+    //     game: 'challenge'; //user 6209ab25b45ca9001665429f
+    //     result: false; // если на указанную дату уже стоит true - не меняем
+    //   }
+    // ]; // массив дат когда слово стало изученным
     counter: number; // для выставления признака изученного слова, инкремент при правильном ответе, обнуление при неправильном
     games: {
       sprint: {
