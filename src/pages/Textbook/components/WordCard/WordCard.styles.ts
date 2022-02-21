a3b22f947e956881f3aa2f34a57fbb3fb28491fd
fgriff <anglirum@gmail.com>
2022-02-21 12:49:47 +0300

    refactor: some styles for word cards

--- src/pages/Textbook/components/WordCard/WordCard.styles.ts
+++ src/pages/Textbook/components/WordCard/WordCard.styles.ts
@@ -2,13 +2,16 @@ import { IUserWord } from 'model/IUserWord';
 import styled from 'styled-components';
 
 export const StyledWordCard = styled.div<{ background?: IUserWord | undefined }>`
-  width: 100%;
-  padding: 30px 20px;
+  width: 450px;
+  /* padding: 0px 0px; */
   margin-bottom: 15px;
   box-shadow: 0px 0px 5px #000;
   border-radius: 5px;
   display: flex;
-  row-gap: 15px;
+  /* row-gap: 15px; */
+  flex-direction: column;
+  /* justify-content: center; */
+  /* align-items: center; */
   background-color: ${({ background }) =>
     background?.difficulty === 'difficult'
       ? 'tomato'
--- src/pages/Textbook/components/WordCard/components/WordImage/WordImage.styles.ts
+++ src/pages/Textbook/components/WordCard/components/WordImage/WordImage.styles.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 export const StyledWordImage = styled.div`
-  width: 150px;
-  height: 150px;
+  /* width: 150px; */
+  /* height: 150px; */
+  width: 100%;
+  height: 200px;
   border-radius: 5px;
   overflow: hidden;
 
