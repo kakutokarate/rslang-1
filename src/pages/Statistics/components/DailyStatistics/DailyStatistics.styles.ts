3bb09adc528ba8fd77502b17c43a0789c53a963c
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-02-21 14:51:45 +0300

    feat: daily statistics components

---
+++ src/pages/Statistics/components/DailyStatistics/DailyStatistics.styles.ts
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+export const StyledDailyStatistics = styled.div``;
---
+++ src/pages/Statistics/components/DailyStatistics/components/MiniGamesDailyStatistics/MiniGamesDailyStatistics.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { Wrapper } from './MiniGamesDailyStatistics.styles';
+
+const MiniGamesDailyStatistics: FC = () => {
+  return (
+    <Wrapper>
+
+    </Wrapper>
+  );
+};
+
+export default MiniGamesDailyStatistics;
