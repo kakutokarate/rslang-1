11ab11fdf5a938ae15dca228d68c058f1ff5ec43
crzoukman <zoukman@yandex.ru>
2022-02-22 19:10:09 +0300

    feat: add sprint loader when starting from textbook

--- src/pages/Sprint/components/SprintGame/SprintGame.tsx
+++ src/pages/Sprint/components/SprintGame/SprintGame.tsx
@@ -4,6 +4,8 @@ import Game from "./components/Game";
 import GameOver from "./components/GameOver";
 import Intro from "./components/Intro";
 import { Wrapper } from "./SprintGame.styled";
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 
 const SprintGame: FC = () => {
   const {
@@ -24,6 +26,13 @@ const SprintGame: FC = () => {
         {isSprintRunning && !isStartedFromTextbook && <Game />}
         {!isSprintRunning && !isStartedFromTextbook && <Intro />}
         {isSprintRunning && isStartedFromTextbook && <Game />}
+        {!isSprintRunning && isStartedFromTextbook && (
+          <section>
+            <Box sx={{ width: '80%' }}>
+              <LinearProgress />
+            </Box>
+          </section>
+        )}
       </Wrapper>
     );
   }
