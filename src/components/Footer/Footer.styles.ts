75a0b0bba0d04f71fd2b95ef6dbb485ba8a88884
crzoukman <zoukman@yandex.ru>
2022-02-22 15:29:57 +0300

    feat: add footer

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -1,5 +1,95 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  margin-top: auto;
+  margin-top: 80px;
+  background-color: #111827;
+  color: white;
+  min-height: 100px;
+  width: 100%;
+  padding: 20px 24px 80px;
+  cursor: default;
+  display: flex;
+  gap: 120px;
 `
+
+export const RSSWrapper = styled.div`
+  width: max-content;
+
+  h2 {
+    font-weight: 600;
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+
+   h3 {
+    font-weight: 400;
+    font-size: 12px;
+   
+  }
+`
+
+export const SVGWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-bottom: 10px;
+
+   svg {
+    cursor: pointer;
+    height: 22px;
+    width: 22px;
+    path {
+      fill: white;
+    }
+    transition: 0.5s;
+
+    &:hover {
+      path {
+      fill: coral;
+    }
+    }    
+  }
+
+  
+`;
+
+export const MenuWrapper = styled.div`
+  h2 {
+    font-weight: 600;
+    font-size: 20px;
+    margin-bottom: 20px; 
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    color: rgba(255, 255, 255, 0.75);
+    transition: 0.5s;
+
+    & > a:hover {
+      color: coral;
+      
+    }
+  }
+`
+
+export const DevelopersWrapper = styled.div`
+   h2 {
+    font-weight: 600;
+    font-size: 20px;
+    margin-bottom: 20px; 
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    color: rgba(255, 255, 255, 0.75);
+    transition: 0.5s;
+
+    & > a:hover {
+      color: coral;
+      
+    }
+  }
+`;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,46 @@
 import { FC } from "react";
-import { Wrapper } from './Footer.styles';
+import { DevelopersWrapper, MenuWrapper, RSSWrapper, SVGWrapper, Wrapper } from './Footer.styles';
+import { ReactComponent as GitSVG } from './assets/images/git.svg';
+import { ReactComponent as YoutubeSVG } from './assets/images/youtube.svg';
+import { ReactComponent as HomeSVG } from './assets/images/home.svg';
+import { Link } from "react-router-dom";
 
 const Footer: FC = () => {
   return (
     <Wrapper>
-      Footer
+      <RSSWrapper>
+        <h2>RSLang</h2>
+        <SVGWrapper>
+          <a href="https://github.com/rolling-scopes-school/tasks/blob/master/tasks/stage-2/rs-lang/rslang.md">
+            <GitSVG />
+          </a>
+          <a href="https://www.youtube.com/c/RollingScopesSchool">
+            <YoutubeSVG />
+          </a>
+          <a href="https://rs.school/">
+            <HomeSVG />
+          </a>
+        </SVGWrapper>
+        <h3>© 2022 RSLang. RS School Stage 2</h3>
+      </RSSWrapper>
+
+      <MenuWrapper>
+        <h2>Меню</h2>
+        <div>
+          <Link to='/'>Главная</Link>
+          <Link to='/textbook'>Учебник</Link>
+          <Link to='/statistic'>Статистика</Link>
+        </div>
+      </MenuWrapper>
+
+      <DevelopersWrapper>
+        <h2>Разработчики</h2>
+        <div>
+          <a href="https://github.com/crzoukman">crzoukman</a>
+          <a href="https://github.com/fgriff">fgriff</a>
+          <a href="https://github.com/nina-si">nina-si</a>
+        </div>
+      </DevelopersWrapper>
     </Wrapper>
 
   );
